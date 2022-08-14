ed6efd3dac92e5b9afaf600e17464adefaf01ce2
Petrichor_yin <104968041+Petrichor-yin@users.noreply.github.com>
2022-08-14 14:37:21 +0800

    Update 003.cpp

--- search/003.cpp
+++ search/003.cpp
@@ -8,7 +8,7 @@ int binsearch(Sqlist L, int key)
   {
     mid = (low + high) / 2;
     if (L.data[mid] == key)
-      return mid + 1;
+      return mid ;
     else if (L.data[mid] < key)
       low = mid + 1;
     else
@@ -27,11 +27,11 @@ int bi(Sqlist L, int key, int low, int high)
   else if (L.data[mid] > key)
     bi(L, key, low, mid - 1);
   else
-    return mid + 1;
+    return mid;
 }
 int main()
 {
   Sqlist L = {{1, 3, 5, 8, 9}, 5};
-  cout << "Î»ÖÃÎª:" << bi(L, 8, 0, 4);
+  cout << "result is:" << bi(L, 8, 0, 4);
   return 0;
 }
