9f1e9193f8be4e0a7c4b40486df6d64ee8ca3047
Petrichor_yin <104968041+Petrichor-yin@users.noreply.github.com>
2022-08-21 18:57:23 +0800

    Update 034.cpp

--- BiTree/034.cpp
+++ BiTree/034.cpp
@@ -4,23 +4,13 @@ using namespace std;
 
 TNode *fun(TNode *T, TNode *p)
 {
-  TNode *q;
-  if (p->rtag == 0)
-    q = p->rchild;
-  else if (p->ltag == 0)
-    q = p->lchild;
-  else if (p->lchild == NULL)
-    q = NULL;
-  else
-  {
-    while (p->ltag == 1 && p->lchild != NULL)
-      p = p->lchild;
-    if (p->ltag == 0)
-      q = p->lchild;
-    else
-      q = NULL;
+  if (p->ltag==0){
+    while(p->rtag==0)
+      p=p->rchild;
+    return p;
   }
-  return q;
+  else 
+    return p->lchild;
 }
 
 int main()
@@ -34,6 +24,6 @@ int main()
     pre->rchild = NULL;
     pre->rtag = 1;
   }
-  cout << "前驱节点为:" << fun(T, p)->data << endl;
+  cout << "pre is:" << fun(T, p)->data << endl;
   return 0;
 }
